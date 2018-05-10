e8e0fe17983355e35e9d0c2e6c07063009809945
cawolf <camilozambranovotto@hotmail.com>
2018-05-10 11:47:46 -0500

    Fixed bug with meteor old files :bug:

--- server/main.js
+++ server/main.js
@@ -1,7 +1,5 @@
-import { Meteor } from "meteor/meteor";
-import "../imports/api/rate-limit.js";
-import "../imports/api/notificaciones.js";
-import "../imports/api/publicaciones.js";
+import { Meteor } from 'meteor/meteor';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
